3e7b017e1305ef0bafbcf99febecea1fc82f2e64
Pete Droll <pete@dynamit.com>
2016-08-17 22:28:22 -0400

    Normalized whitespace.

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ let app = Express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send(`
-  		<h1>Hello World</h1>
-  		<h2>from a Node.js Server written in ECMAScript 6!</h2>
-  		<p>Start editing <code>server.js</code> to make your app come to life!</p>`);
+	res.send(`
+		<h1>Hello World</h1>
+		<h2>from a Node.js Server written in ECMAScript 6!</h2>
+		<p>Start editing <code>server.js</code> to make your app come to life!</p>`);
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${app.get('port')}`);
+	console.log(`Express server listening on port ${app.get('port')}`);
 });
